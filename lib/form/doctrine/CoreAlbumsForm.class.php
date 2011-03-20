4bd17d7e79cbe9e76018bc6258df5dd8107dde34
Will Killian <i@forkmantis.com>
2011-03-20 18:33:53 -0500

    Added new schema:
    Added local database with a table for tracking Plays, and added
    CoreAlbums + relationships to banshee schema.

---
+++ lib/form/doctrine/CoreAlbumsForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * CoreAlbums form.
+ *
+ * @package    banshee
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class CoreAlbumsForm extends BaseCoreAlbumsForm
+{
+  public function configure()
+  {
+  }
+}
