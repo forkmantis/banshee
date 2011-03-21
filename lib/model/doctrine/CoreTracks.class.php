de678b1726f1f2c4db5bfa36b76ae08b985c745f
Will Killian <i@forkmantis.com>
2011-03-20 22:13:14 -0500

    Get Data and Update.
    Added lastfm:get-recent-tracks task, and beefed up the CoreTracks model
    with logic to know when and how to update a record.

--- lib/model/doctrine/CoreTracks.class.php
+++ lib/model/doctrine/CoreTracks.class.php
@@ -12,4 +12,63 @@
  */
 class CoreTracks extends BaseCoreTracks
 {
+  /**
+   * Get the last played timestamp, converted to GMT
+   **/
+  public function getGmLastPlayedStamp()
+  {
+    return $this->getLastPlayedStamp() + (-1 * 
+      (
+        $this->getTimeZoneOffset() + $this->getDaylightSavingsOffset($this->LastPlayedStamp)
+      )
+    );
+  }
+
+  public function getTimeZoneOffset()
+  {
+    return date('Z');
+  }
+
+  public function getDaylightSavingsOffset($timestamp)
+  {
+    return date('I', $timestamp) * 60 * 60;
+  }
+
+  /**
+   * Takes a Recent Track record (as SimpleXMLObject) and determines if the
+   * playcount and last play date needs to be incremented.
+   *
+   * @return bool
+   **/
+  public function updateFromLastFmPlay($xml, $allowableOffset)
+  {
+    $lastFmDate = strtotime($xml->date);    
+    if ($lastFmDate === false) $lastFmDate = date('U');
+
+    if ($this->isAMatchOnTime($lastFmDate, $allowableOffset))
+    {
+      return false;
+    }
+
+    $this->PlayCount++;
+    $this->setLastPlayedStamp($lastFmDate
+      + $this->getTimeZoneOffset()
+    );
+
+    $this->save();
+
+    return true;
+  }
+
+  /**
+   * Checks if the date reported by LastFM matches the songs date within an
+   *  acceptable range
+   *
+   * @return bool
+   **/
+  private function isAMatchOnTime($lastFmDate, $allowableOffset)
+  {
+    return ($lastFmDate >= $this->getGmLastPlayedStamp() - $allowableOffset)
+      && ($lastFmDate <= $this->getGmLastPlayedStamp() + $allowableOffset);
+  }
 }
---
+++ lib/task/getrecenttracksTask.class.php
@@ -0,0 +1,94 @@
+<?php
+
+class getrecenttracksTask extends sfBaseTask
+{
+  protected function configure()
+  {
+    $this->addOptions(array(
+      new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'frontend'),
+      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+      new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'doctrine'),
+      // add your own options here
+      new sfCommandOption('from_date', null, sfCommandOption::PARAMETER_OPTIONAL, 'Date you want to retreive music for (in any valid format)', null),
+      new sfCommandOption('time_offset', null, sfCommandOption::PARAMETER_OPTIONAL, 'How far back from the End Date (in seconds) to grab data', (24 * 60 * 60) - 1),
+      new sfCommandOption('limit', null, sfCommandOption::PARAMETER_OPTIONAL, 'Maximum number of records to retreive', 200),
+    ));
+
+    $this->namespace        = 'lastfm';
+    $this->name             = 'get-recent-tracks';
+    $this->briefDescription = 'retreive recently listened tracks';
+    $this->detailedDescription = <<<EOF
+The [recent-tracks|INFO] task retreive recently listened tracks.
+Call it with:
+
+  [php symfony test|INFO]
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    // initialize the database connection
+    $databaseManager = new sfDatabaseManager($this->configuration);
+    $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+
+  $from = date('U', strtotime(
+    (($options['from_date'] == null) 
+      ? date('Y-m-d').' 00:00:00' 
+      : $options['from_date'])
+    ));
+  $to = $from + $options['time_offset'];
+
+    $lastfm = new wpLastFm();
+    $result = $lastfm->get('user.getRecentTracks'
+      , array(
+        'limit' => $options['limit']
+        , 'from' => $from
+        , 'to' => $to
+      )
+    );
+
+    foreach ($result->recenttracks->track as $track) {
+      $songs = CoreTracksTable::fetchByArtistAlbumTitle(
+        $track->artist
+        , $track->album
+        , $track->name
+      );
+
+      $play = new Play();
+      $play->artist = $track->artist;
+      $play->name = $track->name;
+      $play->album = $track->album;
+      $play->gmt_date = $track->date;
+      
+      if ($songs->Count() == 1)
+      {
+        $song = $songs->getFirst();
+        $prevCount = $song->PlayCount;
+        $prevDate = $song->LastPlayedStamp;
+
+        if ($song->updateFromLastFmPlay($track, sfConfig::get('allowable_offset_seconds', 300)))
+        {
+          $play->track_id = $song->TrackID;
+          $play->status = 'Updated';
+          echo 'UPDATING '.$song->Title.' by '.$song->Album->ArtistName."\n";
+          echo '  playcount set to '.$song->PlayCount.' from '.$prevCount."\n";
+          echo '  lastPlayStamp set to '.date('Y-m-d H:i:s T', $song->LastPlayedStamp).' from '.date('Y-m-d H:i:s', $prevDate)."\n";
+          echo "\n";
+        }
+        else
+        {
+          $play->status = 'Banshee';
+          echo 'NO UPDATE MADE for '.$song->Title.' by '.$song->Album->ArtistName."\n";
+          echo "\n";
+        }
+      }
+      elseif ($songs->count() == 0)
+      {
+        $play->status = 'No Match';
+        print 'NO MATCH FOUND FOR '.$track->name.' by '.$track->artist.' on '.$track->album."\n";
+        print "\n";
+      }
+      $play->save();
+    }
+  }
+}
