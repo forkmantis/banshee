ae26a63861f6bfca597f93d238ce7255bf3427f0
Will Killian <i@forkmantis.com>
2011-03-22 12:27:41 -0500

    Added CoreArtists schema and models.

---
+++ lib/form/doctrine/CoreArtistsForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * CoreArtists form.
+ *
+ * @package    banshee
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class CoreArtistsForm extends BaseCoreArtistsForm
+{
+  public function configure()
+  {
+  }
+}
