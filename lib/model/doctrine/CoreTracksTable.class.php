ba7fe124d43afe9f8778ce439c64b63f9ea53171
Will Killian <i@forkmantis.com>
2011-03-26 08:27:37 -0500

    Added fetchLowestPlayedByRatioAndRating to CoreTracksTable.

--- lib/model/doctrine/CoreTracksTable.class.php
+++ lib/model/doctrine/CoreTracksTable.class.php
@@ -74,8 +74,28 @@ class CoreTracksTable extends Doctrine_Table
           (ct.PlayCount + ct.SkipCount) / ct.Rating) as Ratio')->
         from('CoreTracks ct')->
         innerJoin('ct.Artist a')->
-        where('ct.UriType IS NULL AND ct.LastPlayedStamp < '.(gmdate('U') - $offsetSeconds))->
+        where('(ct.UriType IS NULL OR ct.UriType = 1) AND ct.LastPlayedStamp < '.(gmdate('U') - $offsetSeconds))->
         orderBy('(1000 * (ct.PlayCount + ct.SkipCount) / ct.Rating), 
+          ct.LastPlayedStamp')->
+        limit($limit)->
+        execute()->toArray();
+     }
+
+    /**
+     * Fetch those w/ the lowest play to rating ratio by rating, sorted by how long ago 
+     *  it was played
+     *
+     * @return Array
+     **/
+    public static function fetchLowestPlayedByRatioAndRating($rating, $offsetSeconds, $limit = 100)
+    {
+      return Doctrine_Query::create()->
+        select('a.Name, ct.Title, ct.TrackID, ct.Rating')->
+        from('CoreTracks ct')->
+        innerJoin('ct.Artist a')->
+        where('(ct.UriType IS NULL OR ct.UriType = 1) AND ct.Rating = ? AND ct.LastPlayedStamp < '.(gmdate('U') - $offsetSeconds)
+          , array($rating))->
+        orderBy('(ct.PlayCount + (ct.SkipCount * 2)), 
           ct.LastPlayedStamp')->
         limit($limit)->
         execute()->toArray();
